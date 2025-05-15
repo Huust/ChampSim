ea2bb6eb7674768caf6d2497e6e9388608aefa0b
胡松涛 <ryan@husongtaodeMacBook-Pro.local>
2025-05-16 00:39:05 +0200

    modify comment

--- inc/dram_controller.h
+++ inc/dram_controller.h
@@ -193,7 +193,7 @@ class MEMORY_CONTROLLER : public champsim::operable
   using channel_type = champsim::channel;
   using request_type = typename channel_type::request_type;
   using response_type = typename channel_type::response_type;
-  std::vector<channel_type*> queues;  // upper level requests from different upper level channels
+  std::vector<channel_type*> queues;  // upper level request queue from different upper level channels
   const champsim::data::bytes channel_width;
 
   void initiate_requests();
